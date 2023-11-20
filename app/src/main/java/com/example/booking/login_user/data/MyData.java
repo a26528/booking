ccb072b9281560cb22d20a406404c5d832d0ac56
LeyreDiaz <a26528@svalero.com>
2023-11-20 11:22:23 +0100

    gradel

---
+++ app/src/main/java/com/example/booking/login_user/data/MyData.java
@@ -0,0 +1,24 @@
+package com.example.booking.login_user.data;
+
+
+
+import com.example.booking.beans.User;
+
+import java.util.ArrayList;
+
+public class MyData {
+    private String message;
+    private ArrayList<User> lstUsers;
+
+
+    public String getMessage() {
+        return message;
+    }
+    public ArrayList<User> getLstUsers() {
+        return lstUsers;
+    }
+    public void setLstUsers(ArrayList<User> lstUsers) {
+        this.lstUsers = lstUsers;
+    }
+}
+
